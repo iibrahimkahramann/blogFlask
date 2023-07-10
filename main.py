602975aa0ebbb510ef36c8a770a35ca5e7e147f4
ibrahmjk1903 <110205635+ibrahmjk1903@users.noreply.github.com>
2023-07-10 16:35:38 +0300

    güncelleme işlemi yapıldı

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
+from datetime import datetime
 import sqlite3
+
 
 
 app = Flask(__name__)
@@ -18,7 +20,8 @@ def blog():
             'title': row[1],
             'summary': row[2],
             'contents': row[3],
-            'release_date': row[4]
+            'release_date': row[4],
+            'update_time' : row[5]
         })
 
     conn.close()
@@ -33,13 +36,50 @@ def add():
     contents = request.form['contents']
     release_date = request.form['release_date']
 
+
+    release_date = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
     conn = sqlite3.connect('blog.db')
     cursor = conn.cursor()
     cursor.execute('INSERT INTO posts (title, summary, contents, release_date) VALUES (?,?,?,?)', (title, summary, contents, release_date))
     id = cursor.lastrowid
     conn.commit()
     conn.close()
-    return jsonify({'id': id})
+    return jsonify({'id': id},{'title': title},{'summary': summary},{'contents': contents},{'release_date': release_date })
+
+
+
+@app.route('/update/<int:id>', methods=['POST'])
+def update_record(id):
+    new_title = request.form['title']
+    new_summary = request.form['summary']
+    new_contents = request.form['contents']
+    release_date = request.form['release_date']
+    update_time = request.form['update_time']
+    
+
+
+    update_time = datetime.now().strftime(" %d-%m-%Y %H:%M:%S")
+
+    if new_title and new_summary:
+        conn = sqlite3.connect('blog.db')
+        cursor = conn.cursor()
+
+        cursor.execute('UPDATE posts SET title = ?, summary = ?, contents = ?, update_time = ? WHERE id = ?', (new_title, new_summary, new_contents, update_time,id))
+        conn.commit()
+
+        if cursor.rowcount > 0:
+            return jsonify({'msg': 'Başarıyla güncellendi'})
+        else:
+            return 'Kayıt bulunamadı.'
+    cursor.close()
+    conn.close()
+
+
+
+
+    
+
 
 
 
